d352845e008dd3c0824b48804b8324db55d8b9e5
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2024-06-02 15:31:03 +0300

    Done

---
+++ app.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click',()=>{
+    alert("Waad ku mahadsantahay Macmiil codsiga aad nooso dirtay dhawaan ayan kaaga soo jawaabaynaa insha allah .")
+})
